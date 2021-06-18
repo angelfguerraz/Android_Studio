f05208bdde9382290fe4866ff3932358e422e5a0
Angel Zambrano <angelfguerraz@web.de>
2021-06-18 08:35:33 +0200

    updates

--- PlatziConf/app/src/main/java/com/afgzb/platziconf/ui/activities/SplashscreenActivity.kt
+++ PlatziConf/app/src/main/java/com/afgzb/platziconf/ui/activities/SplashscreenActivity.kt
@@ -1,7 +1,8 @@
-package com.afgzb.platziconf.ui
+package com.afgzb.platziconf.ui.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.afgzb.platziconf.R
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
