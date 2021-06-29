d40ad671b34ebf59cee644243ad6cb5f52e535c7
Angel Zambrano <angelfguerraz@web.de>
2021-06-29 23:31:02 +0200

    A LOT OF FILES

--- PlatziConf/app/src/main/java/com/afgzb/platziconf/MainActivity.kt
+++ PlatziConf/app/src/main/java/com/afgzb/platziconf/MainActivity.kt
@@ -2,10 +2,23 @@ package com.afgzb.platziconf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.afgzb.platziconf.model.Conference
+import com.afgzb.platziconf.model.Speaker
+import com.google.firebase.firestore.FirebaseFirestore
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.*
+
+/*
+* var is like general variable and it's known as a mutable variable in kotlin and can be assigned multiple times.
+
+val is like Final variable and it's known as immutable in kotlin and can be initialized only single time.
+* */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
