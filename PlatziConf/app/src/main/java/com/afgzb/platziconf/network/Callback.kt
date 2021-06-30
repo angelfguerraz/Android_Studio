0761164b7364759d0f2192ec661cbfa60f2c9ae0
Angel Zambrano <angelfguerraz@web.de>
2021-06-30 09:07:46 +0200

    a lot of stuff was deleted

--- PlatziConf/app/src/main/java/com/afgzb/platziconf/network/Callback.kt
+++ PlatziConf/app/src/main/java/com/afgzb/platziconf/network/Callback.kt
@@ -1,7 +1,16 @@
 package com.afgzb.platziconf.network
 
+import java.lang.Exception
+
 /*Implements functions for usage of external services*/
 interface Callback<T> {
     // Callback<T>, T allows the input of any date type
-    
+
+    fun onSuccess(result: T?) {
+
+    }
+
+    fun onFailed(exception: Exception) {
+
+    }
 }
--- PlatziConf/app/src/main/java/com/afgzb/platziconf/network/FirestoreService.kt
+++ PlatziConf/app/src/main/java/com/afgzb/platziconf/network/FirestoreService.kt
@@ -1,7 +1,12 @@
 package com.afgzb.platziconf.network
 
+import com.afgzb.platziconf.model.Conference
+import com.afgzb.platziconf.model.Speaker
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+
+const val CONFERENCE_COLLECTION_NAME = "conferences"
+const val SPEAKER_COLLECTION_NAME = "speakers"
 
 class FirestoreService {
     // Direct connection to our database
@@ -13,11 +18,25 @@ class FirestoreService {
         firebaseFirestore.firestoreSettings = settings
     }
 
-    fun getSpeakers(callback: Callback<list<Speaker>>) {
-
+    fun getSpeakers(callback: Callback<List<Speaker>>) {
+        firebaseFirestore.collection(SPEAKER_COLLECTION_NAME).orderBy("category").get()
+            .addOnSuccessListener { result ->
+                for (doc in result) {
+                    val list = result.toObjects(Speaker::class.java)
+                    callback.onSuccess(list)
+                    break
+                }
+            }
     }
 
-    fun getSchedule() {
-
+    fun getSchedule(callback: Callback<List<Conference>>) {
+        firebaseFirestore.collection(CONFERENCE_COLLECTION_NAME).get()
+            .addOnSuccessListener { result ->
+                for (doc in result) {
+                    val list = result.toObjects(Conference::class.java)
+                    callback.onSuccess(list)
+                    break
+                }
+            }
     }
 }
